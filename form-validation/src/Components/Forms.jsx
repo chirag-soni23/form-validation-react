6b54d59b2a0c65b59ea6cf1bf7366d461074f6aa
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2024-01-01 17:28:45 +0530

    form-validated

--- form-validation/src/Components/Forms.jsx
+++ form-validation/src/Components/Forms.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 function Forms() {
+    const passwordRegex = /[@#$%^&*()]/
+   
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
-  const handleRegistration = (data) => console.log(data);
+  const handleRegistration = (data) => {
+    console.log(data);
+       reset();
+  };
   const handleError = (errors) => {};
   const registerOptions = {
     name: { required: "Name is required" },
@@ -28,9 +34,10 @@ function Forms() {
           type="text"
           name="name"
           placeholder="Enter Your Name"
-          {...register("name", registerOptions.name)} 
-        /> 
-                <small>{errors?.name && errors.name.message}</small><br/> <br/>
+          {...register("name", registerOptions.name)}
+        />
+        <small>{errors?.name && errors.name.message}</small>
+        <br /> <br />
         <label htmlFor="email">Email</label>
         <input
           type="email"
@@ -38,17 +45,24 @@ function Forms() {
           placeholder="Enter Your Email"
           {...register("email", registerOptions.email)}
         />
-        <small>{errors?.email && errors.email.message}</small><br/> <br/>
-
+        <small>{errors?.email && errors.email.message}</small>
+        <br /> <br />
         <label htmlFor="password">Password</label>
         <input
           type="password"
           placeholder="Enter Password"
           name="password"
-          {...register("password", registerOptions.password)}
+          {...register("password", {
+            ...registerOptions.password,
+            pattern: {
+              value: passwordRegex,
+              message:
+                "Password should contain at least one letter and be at least 8 characters long",
+            },
+          })}
         />
-        <small>{errors?.password && errors.password.message}</small><br/> <br/>
-
+        <small>{errors?.password && errors.password.message}</small>
+        <br /> <br />
         <button type="submit">Submit</button>
       </form>
     </div>
